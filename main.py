5f01b6c761d9585e5b592317cbf30a996f5ad6ce
Brad Schoening <bschoeni@gmail.com>
2021-03-27 12:33:21 -0400

    updated to be more modular for use with tutorial

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ import logging
 from ff3 import FF3Cipher
 from rules import clipl, date_generalize, fpe, redact, us_zip_code
 
-# display data set
+# display data set on CLI
 def display_df(df):
     pd.set_option("display.max_columns", 12)
     pd.set_option("display.width", 120)
-#    print()
     print(df)
     df
 
@@ -27,7 +26,15 @@ def load_csv(fname, dtype={}, date_types=[]):
 # TODO: refactor global 
 date_types = []
 
-def anonymize(df):
+
+class Cipher:
+    instance = None
+
+
+def initialize_fpe(key, tweak, radix = 10):
+    Cipher.instance = FF3Cipher(key, tweak, radix)
+
+def old_anonymize(df):
     # for now, read rules from here instead of .deid file
 
     deid = [
@@ -40,19 +47,16 @@ def anonymize(df):
         '{"column" : "Birth Date", "type" : "Generalize.Date", "format" : "5"}',
         '{"column" : "Acct num", "type" : "Mask", "format" : "5"}'
     ]
+    return anonymize(df, deid)
 
-    # Data rules require type coercion by pandas read_csv
-    date_rules = { "Generalize.Date" }
+def build_rules(rules):
+    rulemap = {}
 
-    rules = {}
-    str_cols = []
-
-    for row in deid:
+    for row in rules:
         # map each row in deid to a tuple
         x = json.loads(row, object_hook= lambda d: namedtuple('X', d.keys()) (*d.values()))
         if x.type == "FF3":
-            c = FF3Cipher(10, key="EF4359D8D580AA4F7F036D6F04FC6A94", tweak="D8E7920AFA330A73")
-            obj = lambda s: fpe(c,s)
+             obj = lambda s: fpe(Cipher.instance,s)
         elif x.type == "Generalize.Date":
             # obj = lambda x: datetime.date(x.year, 1, 1)
             obj = lambda x: date_generalize(x)
@@ -69,17 +73,25 @@ def anonymize(df):
             obj = lambda s: redact(s)
         else:
             raise ValueError(f"Unexpected rule {x.type}")
-        rules[x.column] = obj
+        rulemap[x.column] = obj
 
-    print(rules)
-    print(date_types)
+    #print(rulemap)
+    #print(date_types)
+    return rulemap
+
+def anonymize(df, rulelist):
+
+    # Data rules require type coercion by pandas read_csv
+    # date_rules = { "Generalize.Date" }
+
+    df = df.copy()
+    rulemap = build_rules(rulelist)
 
     # iterate over the bound rules, appying them to columns
-
-    for colname,functor in rules.items():
+    for colname, functor in rulemap.items():
         if colname in df.columns:
-            logging.info("anonymizing %s", colname)
-            df[colname] = df[colname].apply(rules[colname])
+            logging.info("anonymize %s", colname)
+            df[colname] = df[colname].apply(rulemap[colname])
         else:
             logging.warning("missing column %s", colname)
 
@@ -89,7 +101,7 @@ def anonymize(df):
 if __name__ == "__main__":
     # TODO: read rules for csv data types
     df=load_csv('../demo/CCSampleData.csv', {"Zipcode": str}, date_types=['Birth Date'])
-    # TODO: refactor into read rules and process data
+    # TODO: refactor into load rules and process data
     out_df = anonymize(df)
     display_df(df)
     df.to_csv('../demo/CCSampleDataOut.csv')
